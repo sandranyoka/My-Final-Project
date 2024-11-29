310aa6e47a38188dff1f2047c1bea5df841dce16
Sandra Nyoka <sandranyoka93@gmail.com>
2024-11-29 22:47:34 +0200

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-let searchform
+function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput=document.querySelector("search-form-input");
+    let weatherAppCityElement=document.querySelector("#weather-app-city");
+    weatherAppCityElementcityElement.innerHTML=searchInput.value;
+}
+
+let searchFormElement =document.querySelector("#search-form");
+searchFormElement.addEventListener("submit",handleSearchSubmit);

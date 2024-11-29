fba7ff6012c470b5b43458def551d7c2ca5083bc
Sandra Nyoka <sandranyoka93@gmail.com>
2024-11-29 23:21:16 +0200

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 function handleSearchSubmit(event){
     event.preventDefault();
-    let searchInput=document.querySelector("search-form-input");
+    let searchInput=document.querySelector("#search-form-input");
     let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInputElement.value;
+    cityElement.innerHTML=searchInput.value;
 }
 
 let searchFormElement =document.querySelector("#search-form");

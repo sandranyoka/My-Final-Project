cfff66b0b7b5392df4eee0527107e38c0756675a
Sandra Nyoka <sandranyoka93@gmail.com>
2024-11-30 12:58:50 +0200

    Added search functionality using api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
+function refreshWeather(response){
+    let temperatureElement=document.querySelector("#temperature");
+    let temperature=response.data.current.temperature;
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.city;
+    temperatureElement.innerHTML=Math.round(temperature);
+    
+}
+
+
+
+function searchCity(){
+    let apikey ="fbbeo522008a6t423889d2a000ef043e";
+    let apiURL =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+    axios.get(apiURL).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
-    cityElement.innerHTML=searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement =document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+searchCity("HARARE")
